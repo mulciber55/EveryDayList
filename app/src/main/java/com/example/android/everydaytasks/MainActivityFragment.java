6703c6bf7587f46d41d150300c2c221d558ae197
Borys <borysstach@gmail.com>
2015-09-08 21:57:45 +0200

    Tidy logic of Animations, and add showing/hiding keyboard

--- app/src/main/java/com/example/android/everydaytasks/MainActivityFragment.java
+++ app/src/main/java/com/example/android/everydaytasks/MainActivityFragment.java
@@ -3,11 +3,13 @@ package com.example.android.everydaytasks;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -45,7 +47,42 @@ public class MainActivityFragment extends Fragment {
         //add footer
         LinearLayout addTask = (LinearLayout) inflater.inflate(R.layout.add_task, container, false);
         listView.addFooterView(addTask);
-        listenButtons(addTask);
+        //references to elements from checkbox, preparation to animate
+        final Button plusButtonEditStart = (Button) addTask.findViewById(R.id.plus_button_edit_start);
+        final Button plusButtonEditStop = (Button) addTask.findViewById(R.id.plus_button_edit_stop);
+        final EditText newTaskEditText = (EditText) addTask.findViewById(R.id.new_task_edit_text);
+        final TextView addNewTaskTextBox = (TextView) addTask.findViewById(R.id.add_new_task_text_view);
+        //should change Enter Key on keyboard to "+" (not working on my Samsung?)
+        newTaskEditText.setImeActionLabel("+", KeyEvent.KEYCODE_ENTER);
+
+
+        plusButtonEditStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                animateToRight(plusButtonEditStart,plusButtonEditStop,newTaskEditText,addNewTaskTextBox);
+            }
+        });
+
+        plusButtonEditStop.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                animateToLeft(plusButtonEditStart, plusButtonEditStop, newTaskEditText, addNewTaskTextBox);
+            }
+        });
+
+        //custom listener to Enter Key, it works the same way as plusButtonEditStop
+        newTaskEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (event.getKeyCode() == KeyEvent.KEYCODE_ENTER){
+                    animateToLeft(plusButtonEditStart, plusButtonEditStop, newTaskEditText, addNewTaskTextBox);
+                    return true;
+                }
+                return false;
+            }
+        });
 
         return rootView;
     }
@@ -70,97 +107,103 @@ public class MainActivityFragment extends Fragment {
             return checkBox;
         }
 
-
     }
 
     /**
-     * Method add new checkbox wen plus button is press.
+     * Method animate adding checkbox
      *
-     * @param layout get layout with buttons
+     *@param plusButtonEditStart button on left, slide to right and hide
+     *@param plusButtonEditStop button shows after start button slide
+     *@param newTaskEditText EditText show after slide
+     *@param addNewTaskTextBox Text VIew hide after slide
+     *
      */
-    public void listenButtons(LinearLayout layout) {
+    public void animateToRight(final Button plusButtonEditStart, final Button plusButtonEditStop, final EditText newTaskEditText, final TextView addNewTaskTextBox) {
 
-        //references to UI
-        final Button plusButtonEditStart = (Button) layout.findViewById(R.id.plus_button_edit_start);
-        final Button plusButtonEditStop = (Button) layout.findViewById(R.id.plus_button_edit_stop);
-        final EditText newTaskEditText = (EditText) layout.findViewById(R.id.new_task_edit_text);
-        final TextView addNewTaskTextBox = (TextView) layout.findViewById(R.id.add_new_task_text_view);
+        //reference to Animation
+        final Animation slideAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.plus_button_move_to_right);
 
+        slideAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
 
-        plusButtonEditStart.setOnClickListener(new View.OnClickListener() {
+            }
 
             @Override
-            public void onClick(View v) {
-                //reference to Animation
-                final Animation slideAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.plus_button_move_to_right);
-                //Listener to change Visible of UI elements after animation
-                slideAnimation.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
+            public void onAnimationEnd(Animation animation) {
 
-                    }
+                plusButtonEditStart.setVisibility(View.GONE);
+                plusButtonEditStop.setVisibility(View.VISIBLE);
+                addNewTaskTextBox.setVisibility(View.GONE);
+                newTaskEditText.setVisibility(View.VISIBLE);
+                //show keyboard
+                InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.toggleSoftInputFromWindow(newTaskEditText.getWindowToken(), InputMethodManager.SHOW_FORCED, 0);
+                newTaskEditText.requestFocus();
 
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
+            }
 
-                        plusButtonEditStart.setVisibility(View.GONE);
-                        plusButtonEditStop.setVisibility(View.VISIBLE);
-                        addNewTaskTextBox.setVisibility(View.GONE);
-                        newTaskEditText.setVisibility(View.VISIBLE);
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+        //Start animation
+        plusButtonEditStart.startAnimation(slideAnimation);
+    }
 
-                    }
 
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-                    }
-                });
-                //Start animation
-                v.startAnimation(slideAnimation);
+    /**
+     * Method animate adding checkbox
+     *
+     *@param plusButtonEditStart button on left shows after stop button slide
+     *@param plusButtonEditStop button slide to left and hide
+     *@param newTaskEditText EditText hide after slide
+     *@param addNewTaskTextBox Text VIew show after slide
+     *
+     */
+
+    public void animateToLeft(final Button plusButtonEditStart, final Button plusButtonEditStop, final EditText newTaskEditText, final TextView addNewTaskTextBox) {
+        //reference to Animation
+        final Animation slideAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.plus_button_move_to_left);
+        //Listener to change Visible of UI elements after animation
+        slideAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                plusButtonEditStart.setVisibility(View.VISIBLE);
+                plusButtonEditStop.setVisibility(View.GONE);
+                addNewTaskTextBox.setVisibility(View.VISIBLE);
+                newTaskEditText.setVisibility(View.GONE);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
             }
         });
 
-        plusButtonEditStop.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                //reference to Animation
-                final Animation slideAnimation = AnimationUtils.loadAnimation(getActivity(), R.anim.plus_button_move_to_left);
-                //Listener to change Visible of UI elements after animation
-                slideAnimation.setAnimationListener(new Animation.AnimationListener() {
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-
-                        plusButtonEditStart.setVisibility(View.VISIBLE);
-                        plusButtonEditStop.setVisibility(View.GONE);
-                        addNewTaskTextBox.setVisibility(View.VISIBLE);
-                        newTaskEditText.setVisibility(View.GONE);
-
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-                    }
-                });
-                //check if user type title of Task
-                String textToInsertInCheckbox = newTaskEditText.getText().toString();
-                if (textToInsertInCheckbox.isEmpty()) {
-                    Toast.makeText(getActivity(), getActivity().getString(R.string.empty_task_toast), Toast.LENGTH_LONG).show();
-                } else {
-                    //Start animation
-                    v.startAnimation(slideAnimation);
-                    //add new checkbox
-                    taskList.add(textToInsertInCheckbox);
-                    newTaskEditText.setText("");
-                }
-
-            }
-        });
-
+        String textToInsertInCheckbox = newTaskEditText.getText().toString();
+        //don't allow to add empty task
+        if (textToInsertInCheckbox.isEmpty()) {
+            Toast.makeText(getActivity(), getActivity().getString(R.string.empty_task_toast), Toast.LENGTH_LONG).show();
+        } else {
+            //hide keyboard
+            InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(newTaskEditText.getWindowToken(), 0);
+            //hide EditText (looks beater that way)
+            newTaskEditText.setVisibility(View.INVISIBLE);
+            //Start animation
+            plusButtonEditStop.startAnimation(slideAnimation);
+            //add new checkbox
+            taskList.add(textToInsertInCheckbox);
+            //erase text from EditBox
+            newTaskEditText.clearComposingText();
+            newTaskEditText.setText("");
+        }
     }
 
 
